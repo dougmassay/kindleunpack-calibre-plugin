acf20df08cc8b9501e540645d38da86694c5251d
Doug Massay <douglaslmassay@gmail.com>
2023-07-19 12:55:10 -0400

    Pull in upstream Kindleunpack core updates

--- setup_tools/getkucore.py
+++ setup_tools/getkucore.py
@@ -19,7 +19,7 @@ else:
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 SOURCE_DIR = os.path.dirname(SCRIPT_DIR)
-COMMIT_SHA = 'c8be31a196fd92803f78ad34a3f18d40319bbac5'
+COMMIT_SHA = 'f90cd5d2a9ee49094c4d99b732ad214c9ee2fc7e'
 # REMOTE_URL = 'https://github.com/kevinhendricks/KindleUnpack/archive/master.zip'
 REMOTE_URL = 'https://github.com/kevinhendricks/KindleUnpack/archive/{}.zip'.format(COMMIT_SHA)
 # FILE_NAME = os.path.join(SCRIPT_DIR, REMOTE_URL.split('/')[-1])
